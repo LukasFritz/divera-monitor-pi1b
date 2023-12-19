813b61146b3c35937343733966de91798a4488f0
Lukas <27607734+LukasFritz@users.noreply.github.com>
2023-12-20 00:48:40 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox
-sudo apt-get install --no-install-recommends chromium-browser
+sudo apt-get install midori
