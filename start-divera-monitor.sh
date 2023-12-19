68be2505b44fb92da960f5fff4cb0c26c002d352
Lukas <27607734+LukasFritz@users.noreply.github.com>
2023-12-20 00:50:07 +0100

    Update start-divera-monitor.sh

--- start-divera-monitor.sh
+++ start-divera-monitor.sh
@@ -7,9 +7,5 @@ xset -dpms # turn off display power management system
 xset s noblank # turn off screen blanking
 xset s off # turn off screen saver
 
-# Remove exit errors from the config files that could trigger a warning
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
-
-# Run Chromium in kiosk mode
-chromium-browser  --noerrdialogs --disable-infobars --check-for-update-interval=31536000 --incognito --kiosk $MONITOR_URL
+# Run Midori in Fullscreen mode
+midori -e Fullscreen $MONITOR_URL
